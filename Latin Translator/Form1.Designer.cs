a69a3087af6a2a63a25538ebb8fd0acfd66612e5
Prasana Devi Palanisamy <prasanadevip@gmail.com>
2021-01-04 14:54:13 -0600

    Latin Translator Project added

---
+++ Latin Translator/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace Latin_Translator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSinister = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.txtSinister = new System.Windows.Forms.TextBox();
+            this.txtDexter = new System.Windows.Forms.TextBox();
+            this.txtMedium = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(40, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(377, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Click the Latin words, I will show you English meaning.";
+            // 
+            // btnSinister
+            // 
+            this.btnSinister.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSinister.Location = new System.Drawing.Point(81, 89);
+            this.btnSinister.Name = "btnSinister";
+            this.btnSinister.Size = new System.Drawing.Size(75, 23);
+            this.btnSinister.TabIndex = 1;
+            this.btnSinister.Text = "Sinister";
+            this.btnSinister.UseVisualStyleBackColor = true;
+            this.btnSinister.Click += new System.EventHandler(this.btnSinister_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(81, 134);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Dexter";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(81, 188);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Medium";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // txtSinister
+            // 
+            this.txtSinister.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSinister.Location = new System.Drawing.Point(231, 89);
+            this.txtSinister.Name = "txtSinister";
+            this.txtSinister.Size = new System.Drawing.Size(164, 22);
+            this.txtSinister.TabIndex = 4;
+            // 
+            // txtDexter
+            // 
+            this.txtDexter.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDexter.Location = new System.Drawing.Point(231, 134);
+            this.txtDexter.Name = "txtDexter";
+            this.txtDexter.Size = new System.Drawing.Size(164, 22);
+            this.txtDexter.TabIndex = 5;
+            // 
+            // txtMedium
+            // 
+            this.txtMedium.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMedium.Location = new System.Drawing.Point(231, 188);
+            this.txtMedium.Name = "txtMedium";
+            this.txtMedium.Size = new System.Drawing.Size(164, 22);
+            this.txtMedium.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 325);
+            this.Controls.Add(this.txtMedium);
+            this.Controls.Add(this.txtDexter);
+            this.Controls.Add(this.txtSinister);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnSinister);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Latin Translator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSinister;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox txtSinister;
+        private System.Windows.Forms.TextBox txtDexter;
+        private System.Windows.Forms.TextBox txtMedium;
+    }
+}
+
---
+++ Latin Translator/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Latin_Translator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSinister_Click(object sender, EventArgs e)
+        {
+            txtSinister.Text = "left";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtDexter.Text = "right";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            txtMedium.Text = "center";
+        }
+    }
+}
