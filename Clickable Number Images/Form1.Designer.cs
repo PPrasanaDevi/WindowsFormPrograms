e69de8fa28b2d4edd17e42aff5f355ab9c1affd8
Prasana Devi Palanisamy <prasanadevip@gmail.com>
2021-01-04 15:14:16 -0600

    Clickable Number Image Project added

---
+++ Clickable Number Images/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿
+namespace Clickable_Number_Images
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtOne = new System.Windows.Forms.TextBox();
+            this.txtTwo = new System.Windows.Forms.TextBox();
+            this.txtThree = new System.Windows.Forms.TextBox();
+            this.txtFour = new System.Windows.Forms.TextBox();
+            this.txtFive = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::Clickable_Number_Images.Properties.Resources.Five;
+            this.pictureBox5.Location = new System.Drawing.Point(509, 24);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::Clickable_Number_Images.Properties.Resources.Four;
+            this.pictureBox4.Location = new System.Drawing.Point(391, 24);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Clickable_Number_Images.Properties.Resources.Three;
+            this.pictureBox3.Location = new System.Drawing.Point(264, 24);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Image = global::Clickable_Number_Images.Properties.Resources.Two;
+            this.pictureBox2.Location = new System.Drawing.Point(144, 24);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Image = global::Clickable_Number_Images.Properties.Resources.One;
+            this.pictureBox1.Location = new System.Drawing.Point(24, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(83, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // txtOne
+            // 
+            this.txtOne.Location = new System.Drawing.Point(24, 156);
+            this.txtOne.Name = "txtOne";
+            this.txtOne.Size = new System.Drawing.Size(100, 20);
+            this.txtOne.TabIndex = 5;
+            // 
+            // txtTwo
+            // 
+            this.txtTwo.Location = new System.Drawing.Point(144, 155);
+            this.txtTwo.Name = "txtTwo";
+            this.txtTwo.Size = new System.Drawing.Size(100, 20);
+            this.txtTwo.TabIndex = 6;
+            // 
+            // txtThree
+            // 
+            this.txtThree.Location = new System.Drawing.Point(264, 155);
+            this.txtThree.Name = "txtThree";
+            this.txtThree.Size = new System.Drawing.Size(100, 20);
+            this.txtThree.TabIndex = 7;
+            // 
+            // txtFour
+            // 
+            this.txtFour.Location = new System.Drawing.Point(391, 155);
+            this.txtFour.Name = "txtFour";
+            this.txtFour.Size = new System.Drawing.Size(100, 20);
+            this.txtFour.TabIndex = 8;
+            // 
+            // txtFive
+            // 
+            this.txtFive.Location = new System.Drawing.Point(523, 156);
+            this.txtFive.Name = "txtFive";
+            this.txtFive.Size = new System.Drawing.Size(100, 20);
+            this.txtFive.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(794, 268);
+            this.Controls.Add(this.txtFive);
+            this.Controls.Add(this.txtFour);
+            this.Controls.Add(this.txtThree);
+            this.Controls.Add(this.txtTwo);
+            this.Controls.Add(this.txtOne);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Clickable Number Images";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.TextBox txtOne;
+        private System.Windows.Forms.TextBox txtTwo;
+        private System.Windows.Forms.TextBox txtThree;
+        private System.Windows.Forms.TextBox txtFour;
+        private System.Windows.Forms.TextBox txtFive;
+    }
+}
+
