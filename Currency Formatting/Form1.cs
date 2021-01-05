dc1cb71bebe610d0f5be87c6d258ad3bcb9d3be4
Prasana Devi Palanisamy <prasanadevip@gmail.com>
2021-01-05 16:30:39 -0600

    Exception handling added to the Currency Formatting Project.

--- Currency Formatting/Form1.cs
+++ Currency Formatting/Form1.cs
@@ -19,23 +19,30 @@ namespace Currency_Formatting
 
         private void btnCalculate_Click(object sender, EventArgs e)
         {
+            try
+            {
 
-            decimal originalPrice; //The item's original price.
-            decimal discountPercentage; //The discount Percentage.
-            decimal discountAmount; //The amount of discount.
-            decimal salePrice; //The item's sale price.
+                decimal originalPrice; //The item's original price.
+                decimal discountPercentage; //The discount Percentage.
+                decimal discountAmount; //The amount of discount.
+                decimal salePrice; //The item's sale price.
 
 
 
-            originalPrice = decimal.Parse(txtOriginalPrice.Text);
+                originalPrice = decimal.Parse(txtOriginalPrice.Text);
 
-            discountPercentage = decimal.Parse(txtPercentage.Text);
-            discountPercentage = discountPercentage / 100;
-            discountAmount = originalPrice * discountPercentage;
+                discountPercentage = decimal.Parse(txtPercentage.Text);
+                discountPercentage = discountPercentage / 100;
+                discountAmount = originalPrice * discountPercentage;
 
-            salePrice = originalPrice - discountAmount;
+                salePrice = originalPrice - discountAmount;
 
-            txtSalePrice.Text = salePrice.ToString("c");
+                txtSalePrice.Text = salePrice.ToString("c");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void btnExit_Click(object sender, EventArgs e)
